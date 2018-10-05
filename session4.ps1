f63b5bbf0664386f987d1b820ed4a26a998f0be4
=masoud <masoudmmf@yahoo.com>
2018-10-04 21:02:39 -0700

    session4

---
+++ session4.ps1
@@ -0,0 +1,28 @@
+﻿Set-ExecutionPolicy RemoteSigned
+
+$Files = Get-ChildItem 
+
+$Files[0]
+$files[1]
+
+$Files = Get-ChildItem *g*
+$Files.LastWriteTime = get-date
+
+$file = Get-ChildItem Script.bat
+$file | gm
+$file.LastWriteTime = "2001-01-01"
+$file.IsReadOnly = $true
+
+(gci cord.wav).name
+(gci cord.wav).LastWriteTime = get-date
+(gci (ps explorer).path).length
+
+(get-item * | sort LastwriteTime )[0].name
+
+gci *.txt | foreach {
+    $_.LastWriteTime = "1999-01-01" 
+}
+
+gci *.txt | foreach {$_.IsReadOnly = $false}
+
+gci *.txt | foreach {$_.IsReadOnly = $false ; $_ } | Format-Table name -AutoSize
