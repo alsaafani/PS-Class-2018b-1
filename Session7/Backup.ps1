8e6247c6697d3a5d8cd28cb9cac17b829c5ff970
masoud <masoudmmf@yahoo.com>
2018-10-25 15:50:56 -0700

    Session7

---
+++ Session7/Backup.ps1
@@ -0,0 +1,18 @@
+﻿Remove-Item backup -Force:$true -Confirm:$false 
+New-Item backup -ItemType Directory
+"*"*80
+Write-Host "Saving the following files to the ""Backup"" folder"
+"*"*80
+$BUCount = 0 ; $BUSize =0
+
+
+Get-ChildItem | Where-Object {$_.LastWriteTime -ge "2011-01-01"} |
+ForEach-Object { 
+    Write-Host " " $_.Name
+    $BUSize += $_.Length
+    $BUCount ++
+    $NewFileName = "backup\" + ($_.BaseName) + "_" + $_.LastWriteTime.ToString("yyyy-MM-dd") + ($_.Extension)
+    Copy-Item $_ $NewFileName
+ }
+ Write-Host "Saved:" $BUCount "Files," $BUSize.ToString("n0") "bytes"
+
---
+++ Session7/Backupv1.ps1
@@ -0,0 +1,20 @@
+﻿Remove-Item backup -Force:$true -Confirm:$false 
+New-Item backup -ItemType Directory
+"*"*80
+Write-Host "Saving the following files to the ""Backup"" folder"
+"*"*80
+$BUCount = 0 ; $BUSize =0
+
+
+Get-ChildItem | Where-Object {$_.LastWriteTime -ge "2011-01-01"} |
+ForEach-Object { 
+    Write-Host " " $_.Name
+    $BUSize += $_.Length
+    $BUCount ++
+    $FileName = [io.path]::GetFileNameWithoutExtension($_.Name)
+    $FileType = [io.path]::GetExtension($_.Name)
+    $NewFileName = "backup\$FileName" + "-" + $_.LastWriteTime.ToString("yyyy-MM-dd") + $FileType
+    Copy-Item $_ $NewFileName
+ }
+ Write-Host "Saved:" $BUCount "Files," $BUSize.ToString("n0") "bytes"
+
